2d7b4a1d6c98f4d55da856a007dbcf65a4556d12
pjpcsilva <pauloloucofixe@gmail.com>
2026-09-10 22:49:09 +0100

    Atualizar o app.py

--- app.py
+++ app.py
@@ -628,5 +628,6 @@ def peso_atual():
                            registos=registos, timestamp=timestamp)
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5004, debug=True)
+            app.run(host='0.0.0.0', port=5004, debug=False)
+
 
